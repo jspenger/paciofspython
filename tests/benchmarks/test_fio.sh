ff2d4f7c5c2d7e269f216ffb4c9312b0454139a8
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-04-15 16:28:44 +0200

    using tee to print output to stdout and log

--- tests/benchmarks/test_fio.sh
+++ tests/benchmarks/test_fio.sh
@@ -25,7 +25,7 @@ mkdir mnt1
 timeout 1m python3 ../../../paciofs/paciofslocal.py --mountpoint mnt1 --volume vol1 --logginglevel ERROR &
 sleep 15
 cd mnt1
-fio --name=test --bs=1K --size=10M --readwrite=randrw > ../test.paciofslocal.log
+fio --name=test --bs=1K --size=10M --readwrite=randrw | tee ../test.paciofslocal.log
 cd ..
 umount mnt1
 
@@ -35,14 +35,14 @@ mkdir mnt2
 timeout 1m python3 ../../../paciofs/passthrough.py vol2 mnt2 &
 sleep 15
 cd mnt2
-fio --name=test --bs=1K --size=10M --readwrite=randrw > ../test.passthrough.log
+fio --name=test --bs=1K --size=10M --readwrite=randrw | tee ../test.passthrough.log
 cd ..
 umount mnt2
 
 echo localfs
 mkdir vol3
 cd vol3
-fio --name=test --bs=1K --size=10M --readwrite=randrw > ../test.localfs.log
+fio --name=test --bs=1K --size=10M --readwrite=randrw | tee ../test.localfs.log
 cd ..
 
 exit
