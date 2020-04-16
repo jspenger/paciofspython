998a89a5c3c85749e6978a4471aa1a7ec5eabf1d
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-04-16 14:14:11 +0200

    remove seed after bootstrap

--- deployment/kubernetes/multichain/multichain-deployment.sh
+++ deployment/kubernetes/multichain/multichain-deployment.sh
@@ -5,3 +5,8 @@ kubectl apply -f $SCRIPTPATH/multichainseed-deployment.yaml
 
 # deploy multichain to kubernetes
 kubectl apply -f $SCRIPTPATH/multichain-deployment.yaml
+
+sleep 30
+
+# delete running multichainseed deployment
+kubectl delete --all -f $SCRIPTPATH/multichainseed-deployment.yaml
